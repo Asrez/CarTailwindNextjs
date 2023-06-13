794cb4d4e6cb22fdb26a5dc171a91d0fb00ae8f0
frontdepartment021 <frontdepartment021@gmail.com>
2023-06-13 15:45:49 +0330

    --amend

--- app/components/CarDetails.tsx
+++ app/components/CarDetails.tsx
@@ -35,12 +35,86 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 enterTo="opactiy-100 scale-100"
                 leave="ease-in duration-200"
                 leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0  scale-95" 
+                leaveTo="opacity-0  scale-95"
               >
-                <Dialog.Panel className={"w-full max-w-lg max-h-[90vh] oveflow-y-auto transform rounded-2xl bg-white shadow-xl transition-all flex flex-col gap-5"}>
-                  <button type="button" className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"  onClick={closeModal}>
-                    <Image src="/close.svg" alt="clsoe" width={20}  height={20} className="object-contain"/>
+                <Dialog.Panel
+                  className={
+                    "relative w-full max-w-lg max-h-[90vh] oveflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5"
+                  }
+                >
+                  <button
+                    type="button"
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                    onClick={closeModal}
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="clsoe"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
                   </button>
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className=" relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        priority
+                        alt="car model"
+                        className="object-contain"
+                        fill
+                        src={"/hero.png"}
+                      />
+                    </div>
+                    <div className="flex gap-3 ">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 roudned-lg">
+                        <Image
+                          priority
+                          alt="car model"
+                          className="object-contain"
+                          fill
+                          src={"/hero.png"}
+                        />
+                      </div>
+
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 roudned-lg">
+                        <Image
+                          priority
+                          alt="car model"
+                          className="object-contain"
+                          fill
+                          src={"/hero.png"}
+                        />
+                      </div>
+
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 roudned-lg">
+                        <Image
+                          priority
+                          alt="car model"
+                          className="object-contain"
+                          fill
+                          src={"/hero.png"}
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-xl capitalize">{car.make} {car.model}</h2>
+
+                    <div className="mt-3 flex flex-wrap gap-4">
+                        {
+                            Object.entries(car).map(([key , value])=>(
+                                <div className="flex justify-between gap-5 w-full text-right " key={key}>
+                                    <h4 className="texst-text-gray capitalize">{key.split("_").join(" ")}</h4>
+                                    <p className="text-black-100 font-semibold">{value}</p>
+
+
+
+                                </div>
+                            ))
+                        }
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
