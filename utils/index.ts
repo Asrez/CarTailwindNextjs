2389c5368fd00c63e8a293fbda394b9ce733beca
frontdepartment021 <frontdepartment021@gmail.com>
2023-06-13 16:24:19 +0330

    --amend

--- utils/index.ts
+++ utils/index.ts
@@ -6,7 +6,7 @@ export async function fetchCars() {
       "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
     },
     response = await fetch(
-      "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+      "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
       {
         headers: headers,
       }
